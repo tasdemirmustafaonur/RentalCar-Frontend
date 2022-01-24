195e060fd22ddb028d5aad770ec335154d913bc2
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-24 15:54:20 +0300

    Add Car Model

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from 'src/app/models/car';
 
 @Component({
   selector: 'app-car',
@@ -6,44 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car.component.css'],
 })
 export class CarComponent implements OnInit {
-  product1 = {
-    productId: 1,
-    productName: 'Bardak',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product2 = {
-    productId: 2,
-    productName: 'Laptop',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product3 = {
-    productId: 3,
-    productName: 'Mouse',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product4 = {
-    productId: 4,
-    productName: 'Keyboard',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product5 = {
-    productId: 5,
-    productName: 'Kamera',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
-  ];
+  cars: Car[] = [];
 
   constructor() {}
 
