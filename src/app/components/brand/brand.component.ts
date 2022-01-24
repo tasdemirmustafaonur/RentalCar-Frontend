93c2148a798a56cc1df2001c860e0ac0033483d0
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-24 16:00:22 +0300

    Add Brand Model

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Brand } from 'src/app/models/entities/brand';
 
 @Component({
   selector: 'app-brand',
   templateUrl: './brand.component.html',
-  styleUrls: ['./brand.component.css']
+  styleUrls: ['./brand.component.css'],
 })
 export class BrandComponent implements OnInit {
+  brands: Brand[] = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
