180d6e1e86a53377cf838d548f9476d2451f9c64
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-25 22:10:23 +0300

    Add Color Models

--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-color',
   templateUrl: './color.component.html',
-  styleUrls: ['./color.component.css']
+  styleUrls: ['./color.component.css'],
 })
 export class ColorComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
