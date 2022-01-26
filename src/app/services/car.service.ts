2fdb04a277aed0065b0fc33d65b40dccda29cc3b
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-26 06:53:41 +0300

    Add Car Listing Feature by Brand

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -8,11 +8,17 @@ import { ListResponseModel } from '../models/responseModels/listResponseModel';
   providedIn: 'root',
 })
 export class CarService {
-  apiUrl = 'https://localhost:44313/api/cars/getdetails';
+  apiUrl = 'https://localhost:44313/api/';
 
   constructor(private httpClient: HttpClient) {}
 
   getCars(): Observable<ListResponseModel<Car>> {
-    return this.httpClient.get<ListResponseModel<Car>>(this.apiUrl);
+    let newPath = this.apiUrl + 'cars/getdetails';
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+  }
+
+  getCarsByBrand(brandId: number) {
+    let newPath = this.apiUrl + 'cars/getbybrandid?id=' + brandId;
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
   }
 }
