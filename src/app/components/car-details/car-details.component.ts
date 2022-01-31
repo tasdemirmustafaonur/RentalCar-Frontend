2c579fdd242d7a5d2dc844211d6fcd2c228fc65e
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-31 14:47:20 +0300

    Add 'Add To Cart' Screen

--- src/app/components/car-details/car-details.component.ts
+++ src/app/components/car-details/car-details.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Data } from '@angular/router';
 import { Car } from 'src/app/models/entities/car';
+import { SingleResponseModel } from 'src/app/models/responseModels/singleResponseModel';
 import { CarDetailsService } from 'src/app/services/car-details.service';
 import { CarImagesService } from 'src/app/services/car-images.service';
 import { CarService } from 'src/app/services/car.service';
+import { RentalService } from 'src/app/services/rental.service';
 
 @Component({
   selector: 'app-car-details',
@@ -14,12 +16,20 @@ export class CarDetailsComponent implements OnInit {
   currentCar: Car;
   carsOfCurrentBrand: Car[] = [];
   carDataLoaded: boolean = false;
+  rentDate: string;
+  returnDate: string;
+
+  isCarCanBeRentedNow: boolean = false;
+  rentalPeriod: number;
+  validateRentalDates: boolean = false;
+  rentalConfirmation: SingleResponseModel<boolean>;
 
   constructor(
     private carDetailsService: CarDetailsService,
     private carService: CarService,
     private carImageService: CarImagesService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private rentalService: RentalService
   ) {}
 
   ngOnInit(): void {
@@ -60,5 +70,76 @@ export class CarDetailsComponent implements OnInit {
 
   getImagePath(imagePath: string) {
     return this.carImageService.apiUrl + imagePath;
+  }
+
+  checkIfAnyReservationsBetweenSelectedDates(
+    carId: number,
+    rentDate: string,
+    returnDate: string
+  ) {
+    if (!carId || !rentDate || !returnDate) {
+      return;
+    }
+    this.validateReservationDates(rentDate, returnDate);
+    if (this.validateRentalDates === true) {
+      this.rentalService
+        .checkIfAnyReservationsBetweenSelectedDates(carId, rentDate, returnDate)
+        .subscribe((response) => {
+          this.rentalConfirmation = response;
+        });
+    }
+  }
+
+  validateReservationDates(rentDate: string, returnDate: string) {
+    if (!rentDate || !returnDate) {
+      return;
+    }
+    let newRentDate = this.convertStringToDate(rentDate);
+    let newReturnDate = this.convertStringToDate(returnDate);
+    if (newRentDate >= newReturnDate) {
+      this.validateRentalDates = false;
+    } else {
+      this.validateRentalDates = true;
+    }
+  }
+
+  checkIfCarCanBeRentedNow(carId: number) {
+    this.rentalService.checkIfCanCarBeRentedNow(carId).subscribe((response) => {
+      this.isCarCanBeRentedNow = response.data;
+    });
+  }
+
+  calculateRentalPeriod() {
+    this.rentalPeriod = this.getRentalPeriod(
+      this.convertStringToDate(this.rentDate),
+      this.convertStringToDate(this.returnDate)
+    );
+  }
+
+  addDayToDate(date: Date, addedDay: number) {
+    let newDate = date;
+    newDate.setDate(date.getDate() + addedDay);
+    let returnString = this.formatDate(newDate);
+    return returnString;
+  }
+
+  convertStringToDate(dateString: string) {
+    return new Date(dateString);
+  }
+
+  getRentalPeriod(rentDate: Date, returnDate: Date): number {
+    let hours = Math.abs(returnDate.getTime() - rentDate.getTime());
+    let days = Math.ceil(hours / (1000 * 3600 * 24));
+    return days;
+  }
+
+  getDateNow() {
+    let date = new Date();
+    let returnStr = this.formatDate(date);
+    return returnStr;
+  }
+
+  formatDate(date: Date) {
+    return date.toLocaleDateString().split('.').reverse().join('-');
   }
 }
