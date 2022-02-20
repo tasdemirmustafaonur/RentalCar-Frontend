363d7bdab9c122a3e91648abd8de3b3860c942ed
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-02-21 00:01:46 +0300

    Added Image Delete and Update Method

--- src/app/services/car-images.service.ts
+++ src/app/services/car-images.service.ts
@@ -1,15 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CarImage } from '../models/entities/carImage';
+import { ResponseModel } from '../models/responseModels/responseModel';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CarImagesService {
-  apiUrl = 'https://localhost:44313/';
+  private apiUrl = 'https://localhost:44313/';
 
   constructor(private httpClient: HttpClient) {}
 
   getImagePath(imagePath: string) {
     return this.apiUrl + imagePath;
+  }
+
+  uploadImage(image: File, carId: number): Observable<ResponseModel> {
+    let newPath = this.apiUrl + 'api/carimages/add';
+    const sendForm = new FormData();
+    sendForm.append('carId', JSON.stringify(carId));
+    sendForm.append('carImage', image, image.name);
+    return this.httpClient.post<ResponseModel>(newPath, sendForm);
+  }
+
+  deleteImage(carImage: CarImage): Observable<ResponseModel> {
+    let newPath = this.apiUrl + 'api/carimages/delete';
+    return this.httpClient.post<ResponseModel>(newPath, carImage);
   }
 }
